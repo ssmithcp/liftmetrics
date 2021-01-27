602138b55fbe5a231a9419c70d0cd5c57563a590
Scott Smith <ssmith.cp@gmail.com>
2021-01-26 21:10:05 -0700

    only show strict mode warnings in dev

--- client/src/index.js
+++ client/src/index.js
@@ -3,19 +3,27 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 
+import config from './util/config'
+
 import App from './components/App'
 import store from './store'
 
 import './index.css'
 
+const StrictMode = () => (
+  config.isDev // only show strict mode warnings in devlopment
+    ? React.StrictMode
+    : React.Fragment
+)
+
 const Root = () => (
-  <React.StrictMode>
+  <StrictMode>
     <Provider store={ store }>
       <Router>
         <App />
       </Router>
     </Provider>
-  </React.StrictMode>
+  </StrictMode>
 )
 
 ReactDOM.render(
